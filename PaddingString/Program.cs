ac560c559221ea86999ec6675701d40615419ba6
bartekk3007 <149260538+bartekk3007@users.noreply.github.com>
2024-06-23 23:55:44 +0200

    Add files via upload

---
+++ PaddingString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace PaddingString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = "Bartek";
+
+            string leftText = text.PadLeft(10);
+            Console.WriteLine(leftText);
+            
+            string rightText = text.PadRight(10, '-');
+            Console.WriteLine(rightText);
+
+            Console.WriteLine("0123456789");
+        }
+    }
+}
